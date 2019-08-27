c6fb3ead2572271585d93e2ea6640462c1f2d5c8
Michelangelo <sisma99@gmail.com>
2019-08-27 19:18:54 +0200

    fixing test 💉

--- test/quic/connection_test.exs
+++ test/quic/connection_test.exs
@@ -4,39 +4,29 @@ defmodule QUIC.ConnectionTest do
   doctest QUIC.Connection
 
   test "Start a QUIC connection with start" do
-    {:ok, pid} = QUIC.Connection.start()
-    assert Process.alive?(pid)
-
-    {:ok, socket} = QUIC.Connection.open(pid, 1337)
+    {:ok, socket} = QUIC.Connection.open(1337)
     assert is_port(socket)
 
-    assert QUIC.Connection.open(pid, 1337) == {:error, :eaddrinuse}
-    assert QUIC.Connection.close(pid) == :ok
+    assert QUIC.Connection.open(1337) == {:error, :eaddrinuse}
+    assert QUIC.Connection.close() == :ok
 
     # Give GenServer some time to exit
     :timer.sleep(10)
-
-    assert !Process.alive?(pid)
   end
 
   test "Start a QUIC connection with start_link" do
-    {:ok, pid} = QUIC.Connection.start_link()
-    assert Process.alive?(pid)
-
-    {:ok, socket} = QUIC.Connection.open(pid, 1337)
+    {:ok, socket} = QUIC.Connection.open(1337)
     assert is_port(socket)
 
     # Trapping an exit since we're using start_link
     # and we don't want to take the test process down with us
     Process.flag(:trap_exit, true)
 
-    assert QUIC.Connection.close(pid) == :ok
+    assert QUIC.Connection.close() == :ok
 
     # Give GenServer some time to exit
     :timer.sleep(10)
 
     Process.flag(:trap_exit, false)
-
-    assert !Process.alive?(pid)
   end
 end
